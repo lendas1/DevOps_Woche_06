edada8bccb8c893bc9de5b2979ea12eaa7bfad75
lendas1 <0815konto123@gmail.com>
2024-05-22 13:52:36 +0200

    Anpassungen nach Test 5

--- TestSet/backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
+++ TestSet/backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
@@ -5,13 +5,15 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
+        // Korrektur nach Test 5: Ersetze alle Zeilenumbrüche durch Kommas
+        numbers = numbers.replace("\n", ",");
         if (numbers.contains(",")) {
             String[] nums = numbers.split(",");
             int sum = 0;
             for (String num : nums) {
-                sum += Integer.parseInt(num.trim()); // Korrektur nach Test 4: Summiere alle getrennten Zahlen.
+                sum += Integer.parseInt(num.trim());
             }
-            return sum;
+            return sum; // Summiere alle getrennten Zahlen.
         }
         return Integer.parseInt(numbers);
     }
